dd3806f514ceb9ee0a959ac9ad1c74f274a03dd3
Hackbright Student <info@hackbrightacademy.com>
2014-06-19 17:59:58 -0700

    saved ex2.js

--- ex2.js
+++ ex2.js
@@ -4,12 +4,12 @@ function fibonacci_set(max) {
         var current_fib = 1;
         while (current_fib < max) {
             fib_list.push(current_fib);
-            current_fib = 
-               fib_list[fib_list.length-1] +                         
+            current_fib =
+               fib_list[fib_list.length-1] +
                fib_list[fib_list.length-2];
         }
         return fib_list;
-    }    
+    }
     else {
         return [1, 1];
     }
